a695e74aa1c255e1a0660d6bca98cfdcbb75d39a
hrom3 <al-hrom-3@mail.ru>
2020-08-21 23:19:36 +0300

    первая прога и настройки IDEA

---
+++ src/MyProgram.java
@@ -0,0 +1,9 @@
+public class  MyProgram {
+    public static void main(String[] args) {
+        System.out.println();
+        for (String arg: args){
+            System.out.println("Hello " + arg + "!");
+            System.out.println();
+        }
+    }
+}
