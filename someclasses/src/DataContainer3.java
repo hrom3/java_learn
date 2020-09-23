216e5b04fc562e3759652927441104fe246ea5dd
Aleksandr Ahramovich <al-hrom-3@mail.ru>
2020-09-23 23:52:30 +0300

    add classes to someclasses

---
+++ someclasses/src/DataContainer3.java
@@ -0,0 +1,13 @@
+//package homework04;
+//
+//import java.lang.reflect.ParameterizedType;
+//
+//public class DataContainer3<T> {
+//
+//    private T data;
+//
+//    public DataContainer3() {
+//        this.data = new ((T) ((ParameterizedType) getClass()
+//                .getGenericSuperclass()).getActualTypeArguments()[0]);
+//    }
+//}
---
+++ someclasses/src/GenericEntityDao.java
@@ -0,0 +1,9 @@
+//package homework04;
+//
+//import java.io.Serializable;
+//import java.util.List;
+//
+//public interface GenericEntityDao<T> {
+//    T findById(Serializable id);
+//    List findAll();
+//}
