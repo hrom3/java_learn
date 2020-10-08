281aea6d5e25da6624e146cdf7cd8beca88ba492
hrom3 <al-hrom-3@mail.ru>
2020-10-08 18:23:41 +0300

    add class work 29-09-2020

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # java_learn
 It's repository for my work in IT-Academy 
+UID
+GUID
---
+++ classwork/src/classworkmy/exceptoins/ErrorMain.java
@@ -0,0 +1,21 @@
+package classworkmy.exceptoins;
+
+import oop.Man;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ErrorMain {
+    public static void main(String[] args) {
+        try {
+            List<Man> man = new ArrayList<>();
+            while (1 == 1) {
+                man.add(new Man());
+            }
+        } catch (OutOfMemoryError error) {
+            System.exit(789);
+        }
+        System.out.println("We're working");
+    }
+
+}
